acfa64168a1445f384b8a5c69fbe043bfb69542a
ckf <jingcheng407@gmail.com>
2026-02-02 22:47:08 +0800

    add: log

--- core/validator_forward.py
+++ core/validator_forward.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 # Copyright © 2023 Yuma Rao
-import json
+
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
@@ -15,7 +15,12 @@ import json
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import traceback
 import numpy as np
+from core.env_setting.env_utils import get_env_setting
+from core.protocol import EFProtocol
+from core.utils import get_current_commit_hash, read_latest_success_set_weights_datetime_str
+from bittensor import logging
 
 
 async def forward(validator):
@@ -25,5 +30,17 @@ async def forward(validator):
     Burn 100% of the miner’s emissions.
     """
     owner_uid = 161
-    validator.update_scores(np.array([1.0]), [owner_uid])
+    try:
+        await validator.dendrite(
+            axons=[validator.metagraph.axons[uid] for uid in [owner_uid]],
+            synapse=EFProtocol(input={'validator_uid': validator.uid,
+                                      'validator_version': get_env_setting().validator_version,
+                                      'validator_git_hash': get_current_commit_hash()[:7],
+                                      'last_set_weights_success_time': read_latest_success_set_weights_datetime_str()
+                                      }),
+            deserialize=True,
+        )
+    except Exception:
+        logging.error(traceback.format_exc())
 
+    validator.update_scores(np.array([1.0]), [owner_uid])
